ca8ab08dc0655bb28d8d20569ec0ff226c32a48c
ModdyNerd <nilawaranimesh@gmail.com>
2026-06-30 12:19:02 +0530

    Fix server root path after restructure: __dirname is now src/

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const PORT = 3000;
-const ROOT = path.join(__dirname, 'output');
+const ROOT = path.join(__dirname, '..', 'output');
 
 const MIME = {
   '.html': 'text/html',
